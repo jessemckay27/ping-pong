48c214e5b5273b11f630b1e21eed8e0ae69157bf
Jesse McKay <rosecity27@comcast.net>
2016-10-28 07:37:07 -0700

    add basic html structure, add a single js function that loops through numbers with modus and outputs list items

---
+++ README.md
@@ -0,0 +1,8 @@
+step 1:
+  input number from form
+
+step 2:
+  loop from 1 to number.length
+  push each number into a new array
+  
+step 3:
---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+function pingPong(number) {
+  var numberArray = [];
+  for (i = 1; i <= number; i++)
+    if (i % 15 === 0) {
+      numberArray.push("<li class='ping-pong'>Ping Pong</li>");
+    } else if (i % 5 === 0) {
+      numberArray.push("<li class='pong'>Pong</li>");
+    } else if (i % 3 === 0) {
+      numberArray.push("<li class='ping'>Ping</li>");
+    } else {
+      numberArray.push("<li>" + i + "</li>");
+    }
+    return numberArray;
+}
+
+
+
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+    $("#output").append(pingPong($("#input").val()));
+
+
+  });
+});
