f17793959f9a286a14a6cc82274335c4d27b254b
Jesse McKay <rosecity27@comcast.net>
2016-10-28 09:27:48 -0700

    add a check for number to be no higher than 200

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,27 @@
 function pingPong(number) {
-
-
   var numberArray = [];
-  for (i = 1; i <= number; i++) {
-    if (i % 15 === 0) {
-      numberArray.push("<li class='ping-pong'>Ping Pong</li>");
-    } else if (i % 5 === 0) {
-      numberArray.push("<li class='pong'>Pong</li>");
-    } else if (i % 3 === 0) {
-      numberArray.push("<li class='ping'>Ping</li>");
-    } else {
-      numberArray.push("<li>" + i + "</li>")
+  if (number <= 200) {
+    for (i = 1; i <= number; i++) {
+      if (i % 15 === 0) {
+        numberArray.push("<li class='ping-pong'>Ping Pong</li>");
+      } else if (i % 5 === 0) {
+        numberArray.push("<li class='pong'>Pong</li>");
+      } else if (i % 3 === 0) {
+        numberArray.push("<li class='ping'>Ping</li>");
+      } else {
+        numberArray.push("<li>" + i + "</li>")
+      }
     }
+    return numberArray;  
+  } else {
+    alert("please enter a number between 1 and 1000");
   }
-  return numberArray;
 }
-
-
-
 
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
-    $("#output").append(pingPong($("#input").val()));
+    $("#output").html(pingPong($("#input").val()));
   });
 });
 
@@ -31,26 +30,37 @@ $(document).ready(function() {
 
 
 
-
-// function pingPong(number) {
+// var pingPong = function(number) {
 //   var numberArray = [];
-//   for (i = 1; i <= number; i++) {
-//     if (i % 15 === 0) {
-//       numberArray.push("<li class='ping-pong'>Ping Pong</li>");
-//     } else if (i % 5 === 0) {
-//       numberArray.push("<li class='pong'>Pong</li>");
-//     } else if (i % 3 === 0) {
-//       numberArray.push("<li class='ping'>Ping</li>");
-//     } else {
-//       numberArray.push("<li>" + i + "</li>")
+//   if (number <= 1000) {
+//     for (i = 1; i <= number; i++) {
+//       if (i % 15 === 0) {
+//         // numberArray.push("<li class='ping-pong'>Ping Pong</li>");
+//         numberArray.push("<li class='ping-pong'>Ping Pong</li>");
+//       } else if (i % 5 === 0) {
+//         // numberArray.push("<li class='pong'>Pong</li>");
+//         numberArray.push("<li class='pong'>Pong</li>");
+//       } else if (i % 3 === 0) {
+//         // numberArray.push("<li class='ping'>Ping</li>");
+//         numberArray.push("<li class='ping'>Ping</li>");
+//       } else {
+//         // numberArray.push("<li>" + i + "</li>")
+//         numberArray.push("<li>" + i + "</li>")
+//       }
 //     }
+//     return numberArray;
+//   } else {
+//     alert("please enter a number between 1 and 1000");
 //   }
-//   return numberArray;
 // }
 //
 // $(document).ready(function() {
 //   $("form").submit(function(event) {
 //     event.preventDefault();
-//     $("#output").append(pingPong($("#input").val()));
+//     // refactored this simpler code into a single piece
+//     // var input = $("#input").val();
+//     // var output = pingPong(input);
+//     // $("#output").html(output);
+//     $("#output").html(pingPong($("#input").val()));
 //   });
 // });
